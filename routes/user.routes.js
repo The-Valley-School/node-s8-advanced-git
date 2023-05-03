db8bc8096d54241aa839c35dfc270978680cab7f
Fran Linde Blazquez <franlindebl@gmail.com>
2023-05-03 10:33:49 +0200

    Added pagination for car and user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -13,8 +13,8 @@ router.get("/", async (req, res) => {
   console.log("ME HAN PEDIDO USUARIOS!!");
   try {
     // Asi leemos query params
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
+    const page = req.query.page ? parseInt(req.query.page) : 1;
+    const limit = req.query.limit ? parseInt(req.query.limit) : 10;
     const users = await User.find()
       .limit(limit)
       .skip((page - 1) * limit);
